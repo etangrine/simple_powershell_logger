600e8f7cba4c3c41c7d31f4e2c81c1cbf81e9b24
Eric Tang <eyt7663@g.rit.edu>
2026-04-03 12:53:39 -0400

    modfied log_server.py so the endpoint is reachable

--- simple-powershell_logger/src/simple_powershell_logger/log_server.py
+++ simple-powershell_logger/src/simple_powershell_logger/log_server.py
@@ -2,12 +2,13 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-app.route('/log', methods=['POST'])
+@app.route('/log', methods=['POST'])
 def receive_log():
-
-    if request.is_json():
+    # print("receive")
+    if request.is_json:
         data = request.get_json()
         command = data.get('command')
         hostname = data.get('hostname')
-        print(f"Host {hostname} rand {command}")
+        print(f"Host {hostname} ran {command}")
+        return f"Host {hostname} ran {command}"
         #Incorporate discord webhook
